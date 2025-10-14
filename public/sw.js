476be9ee151b90fad573e02fbe905fc54e17844b
swift1gh-millions <127302850+swift1gh@users.noreply.github.com>
2025-10-14 15:34:39 +0000

    Hotfix: block any /api/analytics calls at runtime (fetch/beacon/xhr) + SW/caches purge on boot

--- public/sw.js
+++ public/sw.js
@@ -17,8 +17,9 @@ self.addEventListener("install", (event) => {
 self.addEventListener("activate", (event) => {
   // Clear all existing caches to avoid stale assets and take control
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k)))).then(
-      () => self.clients.claim()
-    )
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
